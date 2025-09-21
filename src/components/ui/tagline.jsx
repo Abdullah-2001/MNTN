1231bb0032b3c3e43c0ac659fb81d6c5615433ba
Abdullah-2001 <abdullah.320409@gmail.com>
2025-09-21 19:20:54 +0500

    second commit

--- src/components/ui/tagline.jsx
+++ src/components/ui/tagline.jsx
@@ -3,7 +3,7 @@ import { themeColors } from "../../styles/colors";
 const Tagline = ({ tagline }) => {
   return (
     <div className="flex gap-4 items-center">
-      <div className={`w-10 h-1 bg-[${themeColors.accent}] rounded-full`}></div>
+      <div className={`w-10 h-1 bg-[#FBD784] rounded-full`}></div>
       <p className={`text-[${themeColors.accent}] uppercase tracking-widest text-sm md:text-base`}>{tagline}</p>
     </div>
   )
